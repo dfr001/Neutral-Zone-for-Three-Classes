f4f05485a3db66a1739d562a910eb71b1ac87944
dfr001 <95943427+dfr001@users.noreply.github.com>
2023-09-13 16:12:26 -0700

    Add files via upload

---
+++ asymmetric neutral zone.py
@@ -0,0 +1,58 @@
+import numpy as np
+from sklearn.metrics import confusion_matrix
+# for example application
+from sklearn.datasets import make_classification
+from sklearn.linear_model import LogisticRegression
+
+# let's try to speed it up
+def label_func(p0, p1, L):
+    p2 = 1-p0-p1
+    l01 = L[0]; l02 = L[1]; l10 = L[2]; l12 = L[3]; l20 = L[4]; l21 = L[5]
+    out = np.full(len(p0), "N")
+    out[np.where(((p0 - p1 > l01) & (p1 > p2)) | ((p0 - p2 > l02) & (p2 > p1)))] = "0"
+    out[np.where(((p1 - p0 > l10) & (p0 > p2)) | ((p1 - p2 > l12) & (p2 > p0)))] = "1"
+    out[np.where(((p2 - p0 > l20) & (p0 > p1)) | ((p2 - p1 > l21) & (p1 > p0)))] = "2"
+    return out
+
+def asymmetric_neutral_zone(p0,p1,true_labels,alpha):
+    all_comb = np.array(np.meshgrid(range(101),range(101))).reshape(2,101*101).T/100
+    # 01, 02, 10, 12, 20, 21
+    res = []
+    for i in [[0,1,2],[1,0,2],[2,0,1]]:
+        def cond_err_func(L_2, true_lab_vec, index):
+            L = [0 for x in range(6)]
+            L[slice(2*index[0],2*index[0]+2)] = L_2
+            cond_ind = true_lab_vec != index[0]
+            pred_lab = label_func(p0 = p0[cond_ind], p1 = p1[cond_ind], L = L)
+            cond_err_probs = []
+            cond_err_probs.append(L_2[0]);cond_err_probs.append(L_2[1])
+            pred_ind = pred_lab==str(index[0])
+            true_ind1 = true_lab_vec[cond_ind]==index[1]
+            true_ind2 = true_lab_vec[cond_ind]==index[2]
+            cond_err_probs.append(sum(pred_ind[true_ind1])/sum(true_ind1))
+            cond_err_probs.append(sum(pred_ind[true_ind2])/sum(true_ind2))
+            return cond_err_probs
+        res.append(np.apply_along_axis(cond_err_func, 1, all_comb, true_lab_vec=true_labels, index=i))
+    
+    def obj_fun(qwer,a):
+        return (qwer[2]-a[0])**2 + (qwer[3]-a[1])**2
+    L_res = []
+    for i in [0,1,2]:
+        obj_vecs = np.apply_along_axis(obj_fun,1,res[i],a=alpha[slice(2*i,2*i+2)])
+        L_pair = res[i][np.argmin(obj_vecs),[0,1]]
+        L_res.append(L_pair[0]); L_res.append(L_pair[1])
+    
+    pred_neut = label_func(p0 = pred_probs[:,0], p1 = pred_probs[:,1], L = L_res)
+    # return [L_res, confusion_matrix(y.astype(str), pred_neut, normalize='true')[0:3,]]
+    d = dict();
+    d['L'] = L_res
+    d['conf_table'] = confusion_matrix(y.astype(str), pred_neut, normalize='true')[0:3,]
+    return d
+
+
+X, y = make_classification(n_samples=1000, n_features=10, n_informative=5, n_redundant=5, n_classes=3, random_state=1)
+model = LogisticRegression(multi_class='multinomial', solver='lbfgs')
+# model.fit(x, ylab)
+res = model.fit(X, y)
+pred_probs = res.predict_proba(X)
+asymmetric_neutral_zone(p0=pred_probs[:,0], p1=pred_probs[:,1], true_labels=y, alpha=[0.05,0.05,0.1,0.1,0.15,0.15])
